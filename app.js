c0b43d60d0e8712e6ff80e3628a6c0b1dbd43f25
Ganesh Verma <bibekverma21@gmail.com>
2026-04-15 23:47:52 +0530

    add initial files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo').default;
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -19,30 +20,41 @@ const listingsRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"; //MongoDB URL define
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"; //MongoDB URL define
+const dburl = process.env.ATLASDB_URL;
 
-// Database connect
-main()
-  .then(() => {
-    console.log("connect to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
+// DB CONNECT
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dburl);
+  console.log("connect to DB");
 }
+main().catch(err => console.log(err))
 
+// VIEW SETUP
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine('ejs', ejsMate);
+
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+  mongoUrl: dburl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  store,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -62,16 +74,16 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
 
 // use static serialize and deserialize of model for passport session support
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
+  res.locals.currUser = req.user || null;
   next();
 });
 
